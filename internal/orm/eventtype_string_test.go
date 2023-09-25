ed38e759dbd71eade69f3d2d627d36a4abb08ce4
maskpp <maskpp266@gmail.com>
2023-09-25 15:39:16 +0800

    fix bug about event_type

--- internal/orm/eventtype_string_test.go
+++ internal/orm/eventtype_string_test.go
@@ -8,5 +8,6 @@ import (
 
 func TestEventType_String(t *testing.T) {
 	assert.Equal(t, "L1DepositETH", L1DepositETH.String())
-	assert.Equal(t, "L1FinalizeWithdrawETH", L1FinalizeWithdrawETH.String())
+	assert.Equal(t, "L1DepositWETH", L1DepositWETH.String())
+	assert.Equal(t, "L1SentMessage", L1SentMessage.String())
 }
