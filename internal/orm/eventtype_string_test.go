16fb583cc2aa3f32d1f4927d2d8baa6edfba11a6
maskpp <maskpp266@gmail.com>
2023-10-12 17:47:57 +0800

    revert mischange

--- internal/orm/eventtype_string_test.go
+++ internal/orm/eventtype_string_test.go
@@ -10,5 +10,4 @@ func TestEventType_String(t *testing.T) {
 	assert.Equal(t, "L1DepositETH", L1DepositETH.String())
 	assert.Equal(t, "L1DepositWETH", L1DepositWETH.String())
 	assert.Equal(t, "L1SentMessage", L1SentMessage.String())
-	assert.Equal(t, "L1FinalizeWithdrawWETH", L1FinalizeWithdrawWETH.String())
 }
