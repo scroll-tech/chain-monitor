643ef213df298b665f38a5c6f8d18298f7f4d6e7
maskpp <maskpp266@gmail.com>
2023-10-24 14:57:18 +0800

    repair db

--- internal/controller/monitor/repairdb.go
+++ internal/controller/monitor/repairdb.go
@@ -1,35 +1,42 @@
-package orm
+package monitor
 
 import (
-	"fmt"
+	"chain-monitor/bytecode/scroll/L1"
+	"chain-monitor/bytecode/scroll/L2"
+	"chain-monitor/internal/orm"
+	"chain-monitor/internal/utils"
+	"context"
+	"github.com/scroll-tech/go-ethereum"
+	"github.com/scroll-tech/go-ethereum/accounts/abi/bind"
+	"github.com/scroll-tech/go-ethereum/common"
 	"github.com/scroll-tech/go-ethereum/ethclient"
-	"gorm.io/gorm"
+	"math/big"
 )
 
-const batch = 1000
+const batch = 500
 
 // repairEvent this struct is temporarily used for repairing messenger.
 type repairEvent struct {
-	*TxHead
+	*orm.TxHead
 	Amount string
 }
 
 // RepairMessenger repair l1 and l2 messenger db due to 000019 .
-func RepairMessenger(db *gorm.DB) error {
-	if err := repairL1Messenger(db); err != nil {
+func (ch *ChainMonitor) RepairMessenger() error {
+	if err := ch.repairL1Messenger(); err != nil {
 		return err
 	}
-	if err := repairL2Messenger(db); err != nil {
+	if err := ch.repairL2Messenger(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func repairL1Messenger(client *ethclient.Client, db *gorm.DB) error {
+func (ch *ChainMonitor) repairL1Messenger() error {
 	for {
-		var l1Msgs []L1MessengerEvent
-		rTx := db.Model(&L1MessengerEvent{}).
-			Select("number", "tx_hash", "msg_hash", "amount").
+		var l1Msgs []orm.L1MessengerEvent
+		rTx := ch.db.Model(&orm.L1MessengerEvent{}).
+			Select("number", "tx_hash", "msg_hash", "amount", "type").
 			Where("msg_hash = ''").
 			Order("number DESC").Limit(batch)
 		err := rTx.Scan(&l1Msgs).Error
@@ -37,24 +44,33 @@ func repairL1Messenger(client *ethclient.Client, db *gorm.DB) error {
 			return err
 		}
 
-		txHashes, err := fillL1Messenger(client, db, l1Msgs)
+		_, err = ch.fillL1Messenger(l1Msgs)
 		if err != nil {
 			return err
 		}
-		err = db.Transaction(func(tx *gorm.DB) error {
-			wTx := tx.Model(&L1MessengerEvent{}).Select("msg_hash", "amount").Where("tx_hash IN ?", txHashes)
-			return wTx.Updates(&l1Msgs).Error
-		})
-		if err != nil {
+
+		db := ch.db.Begin()
+		for i := range l1Msgs {
+			msg := &l1Msgs[i]
+			wTx := db.Model(&orm.L1MessengerEvent{}).Select("msg_hash", "amount").Where("tx_hash = ?", msg.TxHash)
+			err := wTx.Updates(msg).Error
+			if err != nil {
+				return err
+			}
+		}
+		if err = db.Commit().Error; err != nil {
 			return err
 		}
 	}
 }
 
-func fillL1Messenger(client *ethclient.Client, db *gorm.DB, l1Msgs []L1MessengerEvent) ([]string, error) {
+func (ch *ChainMonitor) fillL1Messenger(l1Msgs []orm.L1MessengerEvent) ([]string, error) {
 	var (
-		txHashes = make([]string, len(l1Msgs))
-		msgs     = make(map[string]*L1MessengerEvent, len(l1Msgs))
+		db        = ch.db
+		client    = ethclient.NewClient(ch.l1watcher.RPCClient())
+		contracts = ch.l1watcher.L1Contracts()
+		txHashes  = make([]string, len(l1Msgs))
+		msgs      = make(map[string]*orm.L1MessengerEvent, len(l1Msgs))
 	)
 	for i := range l1Msgs {
 		msg := &l1Msgs[i]
@@ -63,7 +79,7 @@ func fillL1Messenger(client *ethclient.Client, db *gorm.DB, l1Msgs []L1Messenger
 	}
 
 	var ethEvents []repairEvent
-	rTx := db.Model(&L1ETHEvent{}).Select("tx_hash", "msg_hash", "amount").Where("tx_hash IN ?", txHashes)
+	rTx := db.Model(&orm.L1ETHEvent{}).Select("tx_hash", "msg_hash", "amount").Where("tx_hash IN ?", txHashes)
 	err := rTx.Scan(&ethEvents).Error
 	if err != nil {
 		return nil, err
@@ -75,7 +91,7 @@ func fillL1Messenger(client *ethclient.Client, db *gorm.DB, l1Msgs []L1Messenger
 	}
 
 	var erc20Event []repairEvent
-	rTx = db.Model(&L1ERC20Event{}).Select("tx_hash", "msg_hash", "amount").Where("tx_hash IN ?", txHashes)
+	rTx = db.Model(&orm.L1ERC20Event{}).Select("tx_hash", "msg_hash", "amount").Where("tx_hash IN ?", txHashes)
 	err = rTx.Scan(&erc20Event).Error
 	if err != nil {
 		return nil, err
@@ -86,20 +102,56 @@ func fillL1Messenger(client *ethclient.Client, db *gorm.DB, l1Msgs []L1Messenger
 		delete(msgs, event.TxHash)
 	}
 
-	for _, event := range msgs {
-		if event.MsgHash == "" {
-			fmt.Println(event.TxHash)
+	for _, msg := range msgs {
+		msgBind := bind.NewBoundContract(contracts.ScrollMessenger, *L1.L1ScrollMessengerABI, nil, nil, nil)
+		// get non gateway messenger events.
+		filter := ethereum.FilterQuery{
+			FromBlock: big.NewInt(int64(msg.Number)),
+			ToBlock:   big.NewInt(int64(msg.Number)),
+			Addresses: []common.Address{contracts.ScrollMessenger},
+		}
+		logs, err := client.FilterLogs(context.Background(), filter)
+		if err != nil {
+			return nil, err
+		}
+		for _, log := range logs {
+			if log.TxHash.String() != msg.TxHash {
+				continue
+			}
+			if msg.Type == orm.L1SentMessage {
+				event := new(L1.L1ScrollMessengerSentMessageEvent)
+				if err = msgBind.UnpackLog(event, "SentMessage", log); err != nil {
+					return nil, err
+				}
+				msgHash := utils.ComputeMessageHash(event.Sender, event.Target, event.Value, event.MessageNonce, event.Message)
+				msg.MsgHash = msgHash.String()
+				msg.Amount = event.Value.String()
+			}
+			if msg.Type == orm.L1RelayedMessage {
+				event := new(L1.L1ScrollMessengerRelayedMessageEvent)
+				if err = msgBind.UnpackLog(event, "RelayedMessage", log); err != nil {
+					return nil, err
+				}
+				msg.MsgHash = common.BytesToHash(event.MessageHash[:]).String()
+			}
+			if msg.Type == orm.L1FailedRelayedMessage {
+				event := new(L1.L1ScrollMessengerFailedRelayedMessageEvent)
+				if err = msgBind.UnpackLog(event, "FailedRelayedMessage", log); err != nil {
+					return nil, err
+				}
+				msg.MsgHash = common.BytesToHash(event.MessageHash[:]).String()
+			}
 		}
 	}
 
 	return txHashes, nil
 }
 
-func repairL2Messenger(db *gorm.DB) error {
+func (ch *ChainMonitor) repairL2Messenger() error {
 	for {
-		var l2Msgs []L2MessengerEvent
-		rTx := db.Model(&L2MessengerEvent{}).
-			Select("number", "msg_hash", "amount").
+		var l2Msgs []orm.L2MessengerEvent
+		rTx := ch.db.Model(&orm.L2MessengerEvent{}).
+			Select("number", "msg_hash", "amount", "type").
 			Where("tx_hash = ''").
 			Order("number DESC").Limit(batch)
 		err := rTx.Scan(&l2Msgs).Error
@@ -107,24 +159,33 @@ func repairL2Messenger(db *gorm.DB) error {
 			return err
 		}
 
-		msgHashes, err := fillL2Messenger(db, l2Msgs)
+		_, err = ch.fillL2Messenger(l2Msgs)
 		if err != nil {
 			return err
 		}
-		err = db.Transaction(func(tx *gorm.DB) error {
-			wTx := tx.Model(&L2MessengerEvent{}).Select("tx_hash", "amount").Where("msg_hash IN ?", msgHashes)
-			return wTx.Updates(&l2Msgs).Error
-		})
-		if err != nil {
+
+		db := ch.db.Begin()
+		for i := range l2Msgs {
+			msg := &l2Msgs[i]
+			wTx := db.Model(&orm.L2MessengerEvent{}).Select("tx_hash", "amount").Where("msg_hash = ?", msg.MsgHash)
+			err = wTx.Updates(msg).Error
+			if err != nil {
+				return err
+			}
+		}
+		if err = db.Commit().Error; err != nil {
 			return err
 		}
 	}
 }
 
-func fillL2Messenger(db *gorm.DB, l2Msgs []L2MessengerEvent) ([]string, error) {
+func (ch *ChainMonitor) fillL2Messenger(l2Msgs []orm.L2MessengerEvent) ([]string, error) {
 	var (
+		db        = ch.db
+		contracts = ch.l2watcher.L2Contracts()
+		client    = ethclient.NewClient(ch.l2watcher.RPCClient())
 		msgHashes = make([]string, len(l2Msgs))
-		msgs      = make(map[string]*L2MessengerEvent, len(l2Msgs))
+		msgs      = make(map[string]*orm.L2MessengerEvent, len(l2Msgs))
 	)
 	for i := range l2Msgs {
 		msg := &l2Msgs[i]
@@ -133,23 +194,59 @@ func fillL2Messenger(db *gorm.DB, l2Msgs []L2MessengerEvent) ([]string, error) {
 	}
 
 	var ethEvents []repairEvent
-	rTx := db.Model(&L2ETHEvent{}).Select("tx_hash", "msg_hash", "amount").Where("tx_hash IN ?", msgHashes)
+	rTx := db.Model(&orm.L2ETHEvent{}).Select("tx_hash", "msg_hash", "amount").Where("msg_hash IN ?", msgHashes)
 	err := rTx.Scan(&ethEvents).Error
 	if err != nil {
 		return nil, err
 	}
 	for _, event := range ethEvents {
-		msgs[event.MsgHash].TxHash, msgs[event.MsgHash].Amount = event.TxHash, event.Amount
+		msg := msgs[event.MsgHash]
+		msg.TxHash, msg.Amount = event.TxHash, event.Amount
+		delete(msgs, event.MsgHash)
 	}
 
 	var erc20Event []repairEvent
-	rTx = db.Model(&L2ERC20Event{}).Select("tx_hash", "msg_hash", "amount").Where("tx_hash IN ?", msgHashes)
+	rTx = db.Model(&orm.L2ERC20Event{}).Select("tx_hash", "msg_hash", "amount").Where("msg_hash IN ?", msgHashes)
 	err = rTx.Scan(&erc20Event).Error
 	if err != nil {
 		return nil, err
 	}
 	for _, event := range erc20Event {
-		msgs[event.MsgHash].TxHash, msgs[event.MsgHash].Amount = event.TxHash, event.Amount
+		msg := msgs[event.MsgHash]
+		msg.TxHash, msg.Amount = event.TxHash, event.Amount
+		delete(msgs, event.MsgHash)
 	}
+
+	for _, msg := range msgs {
+		abi := L2.L2ScrollMessengerABI
+		msgBind := bind.NewBoundContract(contracts.ScrollMessenger, *abi, nil, nil, nil)
+		// get non gateway messenger events.
+		filter := ethereum.FilterQuery{
+			FromBlock: big.NewInt(int64(msg.Number)),
+			ToBlock:   big.NewInt(int64(msg.Number)),
+			Addresses: []common.Address{contracts.ScrollMessenger},
+		}
+		logs, err := client.FilterLogs(context.Background(), filter)
+		if err != nil {
+			return nil, err
+		}
+		for _, log := range logs {
+			if msg.Type == orm.L2SentMessage && log.Topics[0] == abi.Events["SentMessage"].ID {
+				event := new(L2.L2ScrollMessengerSentMessageEvent)
+				if err = msgBind.UnpackLog(event, "SentMessage", log); err != nil {
+					return nil, err
+				}
+				msg.TxHash = log.TxHash.String()
+				msg.Amount = event.Value.String()
+			}
+			if msg.Type == orm.L2RelayedMessage && log.Topics[0] == abi.Events["RelayedMessage"].ID {
+				msg.TxHash = log.TxHash.String()
+			}
+			if msg.Type == orm.L2FailedRelayedMessage && log.Topics[0] == abi.Events["FailedRelayedMessage"].ID {
+				msg.TxHash = log.TxHash.String()
+			}
+		}
+	}
+
 	return msgHashes, nil
 }
